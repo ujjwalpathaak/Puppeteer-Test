f5f953a1f0a0a66ec9c14d56563ef587ca0b5c7e
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2024-01-25 10:09:18 +0530

    /

--- controller.careerjet.js
+++ controller.careerjet.js
@@ -13,13 +13,16 @@ const details = {
 
 const setupBrowser = async () => {
     puppeteer.use(StealthPlugin())
-    const browser = await puppeteer.launch({ args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"], headless: true, executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath() }); // Set headless to true for production
+    const browser = await puppeteer.launch({ 
+        args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"], headless: true, executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath() }); // Set headless to true for production
     const page = await browser.newPage();
     return { browser, page };
 }
 
 const startApply = async () => {
+    console.log("Starting application process...");
     const { browser, page } = await setupBrowser();
+    console.log("Setting up browser and opening CareerJet...");
     try {
         await page.goto("https://www.careerjet.co.in/login");
         await page.waitForTimeout(1000);
